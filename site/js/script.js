acd2c06e081f6f180b2585a73c9976cbe0970f70
Andrew Ferrari <andrew.ferrari@sptech.school>
2023-03-22 22:21:45 -0300

    Finalizando a aula 2

--- site/js/script.js
+++ site/js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+const team1 = "FALLEN";
+const team2 = "TRYBE";
+
+let turn = team1;
+
+const turnText = document.querySelector("#turnText");
+turnText.innerHTML = `É a vez da equipe ${turn} banir o mapa`;
+
+const maps = document.querySelectorAll(".card");
+
+function choosemap(event){
+    console.log(event.currentTarget);
+    if(turn == team1){
+        turn = team2;
+    }else{
+        turn = team1;
+    }
+    turnText.innerHTML = `É a vez da equipe ${turn} banir o mapa`;
+}
+
+for(let index = 0; index < maps.length; index++) {
+    maps[index].addEventListener("click",choosemap);
+}
